930c01d6cc8d0147133bc51cdaaf35155798acbb
Shemitaremix <israel_1320114066@uptecamac.edu.mx>
2021-11-13 14:25:45 -0600

    alertas

---
+++ liquorstore-master/js/alert.js
@@ -0,0 +1,91 @@
+
+    $("#alert1").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+          })
+
+        }
+    $("#login").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+        }
+    $("#login2").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
+
+    $("#merch1").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
+
+    $("#merch2").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
+    $("#merch3").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
+    $("#merch4").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
+    $("#merch5").on("click",myFuncion)
+
+    function myFuncion(){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Al parecer estamos en construccion!',
+            footer: '<a href="">Proximamente quedara</a>'
+            })
+    
+    }
